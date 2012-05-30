67b68c0b953e30f99ffe2be137909af4681656f7
mathieul <mathieul@gmail.com>
2012-05-30 01:02:16 -0700

    add a RottenTomatoesMovie model to lookup movies

---
+++ reference-app/app/models/rotten_tomatoes_movie.rb
@@ -0,0 +1,21 @@
+class RottenTomatoesMovie
+  include HTTParty
+  base_uri 'api.rottentomatoes.com/api/public/v1.0'
+  format :json
+  default_params apikey: APP_CONFIG[:rotten_tomatoes_key]
+
+  attr_reader :raw, :id, :title, :year, :rating
+
+  def self.search(name)
+    response = get('/movies.json', query: {q: name, page_limit: 50, page: 1})
+    result = response.to_hash
+    return [] if result['total'] == 0
+    result['movies'].map { |raw| new(raw) }
+  end
+
+  def initialize(raw)
+    @raw = raw
+    @id, @title, @year, ratings = @raw.values_at('id', 'title', 'year', 'ratings')
+    @rating = ratings['critics_score']
+  end
+end
